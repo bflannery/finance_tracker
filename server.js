cde7c7f6d61c9b8959cd81df6f558bdabe89aa6c
Brian Flannery <bflannery66@email.com>
2019-04-05 07:43:10 -0500

    initial commit; users register, login, and auth setup

---
+++ server.js
@@ -0,0 +1,47 @@
+const path = require('path')
+const http = require('http')
+const express = require('express')
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+const passport = require('passport')
+
+const users = require('./routes/api/users')
+
+const app = express()
+
+// Body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+// DB Config
+const db = require('./config/keys').mongoURI
+
+// Connect to MongoDB
+mongoose
+  .connect(db, { useNewUrlParser: true })
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err))
+
+// Passport middleware
+// app.use(passport.initialize())
+
+// Passport config
+// require('./config/passport')(passport)
+
+// Landing Route
+app.get('/', (req, res) => res.send('Hello'))
+
+// User Routes
+app.use('/api/users', users)
+
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static('client/build'))
+
+//   app.get('*', (req, res) => {
+//     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+//   })
+// }
+
+const port = process.env.PORT || 5000
+
+app.listen(port, () => console.log(`Server running on port ${port}`))
