0e0246c3827c09755b041fbbc9fcdbbe2a9b6cd2
Brian Flannery <bflannery66@email.com>
2019-05-06 09:27:36 -0500

    working on delete

--- routes/api/incomes.js
+++ routes/api/incomes.js
@@ -10,54 +10,145 @@ const Portfolio = require('../../models/Portfolio')
 // Load User Model
 const User = require('../../models/User')
 
+// ROUTE METHODS
+
+const getIncome = (req, res) => {
+  const errors = {}
+  Income.findById(req.params.income_id)
+    .then(income => {
+      if (!income) {
+        errors.noIncomes = 'Income does not exist'
+        return res.status(404).json(incomes)
+      }
+      res.json(income)
+    })
+    .catch(err => {
+      errors.noIncomes = 'Income does not exist'
+      res.status(404).json(errors)
+    })
+}
+
+const getPortfolioIncomes = (req, res) => {
+  const errors = {}
+  Income.find({ portfolio: req.params.portfolio_id })
+    .then(incomes => {
+      if (incomes.length === 0) {
+        errors.noIncomes = 'There is no incomes for this portfolio'
+        return res.status(404).json(errors)
+      }
+      res.json(incomes)
+    })
+    .catch(err => {
+      errors.noIncomes = 'There is no incomes for this portfolio'
+      res.status(404).json(errors)
+    })
+}
+
+const createPortfolioIncome = (req, res) => {
+  const errors = {}
+  // Check Validation
+  if (!req.body.source) {
+    errors.name = 'Income source is required'
+    // Return any errors with 400 status
+    return res.status(400).json(errors)
+  }
+
+  // Get fields
+  const incomeFields = {}
+  incomeFields.portfolio = req.params.portfolio_id
+  incomeFields.source = req.body.source
+  incomeFields.amount = req.body.amount
+
+  Income.findOne({ source: req.body.source })
+    .then(income => {
+      if (income) {
+        errors.source = 'An income source already exists with that name.'
+        return res.status(400).json(errors)
+      }
+
+      // Save new income
+      new Income(incomeFields)
+        .save()
+        .then(income => {
+          Portfolio.findById(req.params.portfolio_id)
+            .then(portfolio => {
+              portfolio.income.push(income)
+              portfolio.save()
+            })
+            .catch(err => res.json(err))
+          return res.status(200).json(income)
+        })
+        .catch(err => res.status(404).json(err))
+    })
+    .catch(err => res.status(404).json(err))
+}
+
+const updateIncome = (req, res) => {
+  const errors = {}
+  const reqIncome = {
+    ...req.body,
+    lastUpdated: new Date().toISOString()
+  }
+  Income.findOneAndUpdate(
+    { _id: req.params.income_id },
+    { $set: reqIncome },
+    { new: true }
+  )
+    .then(updatedIncome => res.status(200).json(updatedIncome))
+    .catch(err => res.status(404).json(err))
+}
+
+const deleteIncome = (req, res) => {
+  Income.findByIdAndRemove(req.params.income_id)
+    .then(deletedIncome => {
+      Portfolio.findById(deletedIncome.portfolio)
+        .then(portfolio => {
+          const newIncomes = portfolio.incomes.filter(
+            income => income === deleteIncome._id
+          )
+          portfolio.incomes = newIncomes
+          portfolio.save()
+        })
+        .catch(err => res.json(err))
+      res.status(200).json(deletedIncome)
+    })
+    .catch(err => res.status(404).json(err))
+}
+
+// @route   GET api/incomes/portfolio_id
+// @desc    Get incomes by portoflio id
+// @access  Private
+router.get(
+  '/:portfolio_id',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => getPortfolioIncomes(req, res)
+)
+
 // @route   POST api/incomes/portfolio_id
 // @desc    Create a new income source and amount
 // @access  Private
 router.post(
   '/:portfolio_id',
   passport.authenticate('jwt', { session: false }),
-  (req, res) => {
-    console.log({
-      portfolioId: req.params.portfolio_id,
-      reqBody: req.body
-    })
-    // Check Validation
-    if (!req.body.source) {
-      const errors = {}
-      errors.name = 'Income source is required'
-      // Return any errors with 400 status
-      return res.status(400).json(errors)
-    }
+  (req, res) => createPortfolioIncome(req, res)
+)
 
-    // Get fields
-    const incomeFields = {}
-    incomeFields.portfolio = req.params.portfolio_id
-    incomeFields.source = req.body.source
-    incomeFields.amount = req.body.amount
+// @route   PUT api/incomes/portfolio_id
+// @desc    Update a new income source and amount
+// @access  Private
+router.put(
+  '/:income_id',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => updateIncome(req, res)
+)
 
-    Income.findOne({ source: req.body.source })
-      .then(income => {
-        if (income) {
-          errors.source = 'An income source already exists with that name.'
-          return res.status(400).json(errors)
-        }
-
-        // Save new income
-        new Income(incomeFields)
-          .save()
-          .then(income => {
-            Portfolio.findById(req.params.portfolio_id)
-              .then(portfolio => {
-                portfolio.income.push(income)
-                portfolio.save()
-              })
-              .catch(err => res.json(err))
-            return res.status(200).json(income)
-          })
-          .catch(err => res.status(404).json(err))
-      })
-      .catch(err => res.status(404).json(err))
-  }
+// @route   PUT api/incomes/portfolio_id
+// @desc    Update a new income source and amount
+// @access  Private
+router.delete(
+  '/:income_id',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => deleteIncome(req, res)
 )
 
 module.exports = router
--- routes/api/portfolios.js
+++ routes/api/portfolios.js
@@ -2,7 +2,6 @@ const express = require('express')
 const router = express.Router()
 const mongoose = require('mongoose')
 const passport = require('passport')
-const moment = require('moment')
 
 // Load Validation
 // const validateProfileInput = require('../../validation/portfolio')
@@ -13,15 +12,9 @@ const Portfolio = require('../../models/Portfolio')
 // Load User Model
 const User = require('../../models/User')
 
-// @route   GET api/profile/user/:user_id
-// @desc    Get profile by user ID
-// @access  Public
-
-router.get('/:user_id', (req, res) => {
+// ROUTE METHODS
+const getPortfolio = (req, res) => {
   const errors = {}
-  console.log({
-    userId: req.params.user_id
-  })
   Portfolio.findOne({ user: req.params.user_id })
     .populate('income')
     .then(portfolio => {
@@ -35,7 +28,76 @@ router.get('/:user_id', (req, res) => {
       errors.noPortfolio = 'There is no portfolio for this user'
       res.status(404).json(errors)
     })
-})
+}
+
+const createPortoflio = (req, res) => {
+  // Check Validation
+  if (!req.body.name) {
+    const errors = {}
+    errors.name = 'Portfolio name is required'
+    // Return any errors with 400 status
+    return res.status(400).json(errors)
+  }
+
+  // Get fields
+  const portfolioFields = {}
+  portfolioFields.user = req.params.user_id
+  portfolioFields.name = req.body.name
+
+  // Check for existing portfolio
+  Portfolio.findOne({ name: req.body.name })
+    .then(portfolio => {
+      if (portfolio) {
+        errors.handle = 'A portfolio already exists with that name.'
+        return res.status(400).json(errors)
+      }
+      // Save new portfolio
+      new Portfolio(portfolioFields)
+        .save()
+        .then(portfolio => res.json(portfolio))
+        .catch(err => res.status(404).json(err))
+    })
+    .catch(err => res.status(404).json(err))
+}
+
+const updatePortfolio = (req, res) => {
+  const requestBody = req.body
+  // Check Validation
+  if (!requestBody.name) {
+    const errors = {}
+    errors.name = 'Portfolio name is required'
+    // Return any errors with 400 status
+    return res.status(400).json(errors)
+  }
+
+  // Get fields
+  const requestPortolio = {
+    ...requestBody,
+    lastUpdated: new Date().toISOString()
+  }
+
+  Portfolio.findOneAndUpdate(
+    { user: req.params.user_id },
+    { $set: requestPortolio },
+    { new: true }
+  )
+    .then(portfolio => res.json(portfolio))
+    .catch(err => res.status(404).json(err))
+}
+
+const deletePortfolio = (req, res) => {
+  Portfolio.findByIdAndRemove(req.params.portfolio_id)
+    .then(deletedPortfolio => res.status(200).json(deletedPortfolio))
+    .catch(err => res.status(404).json(err))
+}
+
+// ROUTES
+
+// @route   GET api/profile/user/:user_id
+// @desc    Get profile by user ID
+// @access  Public
+
+router.get('/:user_id', (req, res) => getPortfolio(req, res))
 
 // @route   POST api/portfolios/user_id
 // @desc    Create a portfolio
@@ -43,41 +105,7 @@ router.get('/:user_id', (req, res) => {
 router.post(
   '/:user_id',
   passport.authenticate('jwt', { session: false }),
-  (req, res) => {
-    console.log({
-      userId: req.user.id,
-      paramsId: req.params.user_id,
-      reqBody: req.body
-    })
-
-    // Check Validation
-    if (!req.body.name) {
-      const errors = {}
-      errors.name = 'Portfolio name is required'
-      // Return any errors with 400 status
-      return res.status(400).json(errors)
-    }
-
-    // Get fields
-    const portfolioFields = {}
-    portfolioFields.user = req.params.user_id
-    portfolioFields.name = req.body.name
-
-    // Check for existing portfolio
-    Portfolio.findOne({ name: req.body.name })
-      .then(portfolio => {
-        if (portfolio) {
-          errors.handle = 'A portfolio already exists with that name.'
-          return res.status(400).json(errors)
-        }
-        // Save new portfolio
-        new Portfolio(portfolioFields)
-          .save()
-          .then(portfolio => res.json(portfolio))
-          .catch(err => res.status(404).json(err))
-      })
-      .catch(err => res.status(404).json(err))
-  }
+  (req, res) => createPortoflio(req, res)
 )
 
 // @route   PUT api/portfolios/user_id
@@ -86,106 +114,16 @@ router.post(
 router.put(
   '/:user_id',
   passport.authenticate('jwt', { session: false }),
-  (req, res) => {
-    // Check Validation
-    if (!req.body.name) {
-      const errors = {}
-      errors.name = 'Portfolio name is required'
-      // Return any errors with 400 status
-      return res.status(400).json(errors)
-    }
-
-    // Get fields
-    const portfolioFields = {}
-    portfolioFields.user = req.params.user_id
-    if (req.body.name) portfolioFields.name = req.body.name
-
-    Portfolio.findOneAndUpdate(
-      { user: req.user.id },
-      { $set: portfolioFields },
-      { new: true }
-    )
-      .then(portfolio => res.json(portfolio))
-      .catch(err => res.status(404).json(err))
-  }
-)
-
-// @route   POST api/portfolio/user_id/income
-// @desc    Add experience to portfolio
-// @access  Private
-router.post(
-  '/:user_id/income',
-  passport.authenticate('jwt', { session: false }),
-  (req, res) => {
-    const { errors, isValid } = validateExperienceInput(req.body)
-
-    // Check Validation
-    if (!isValid) {
-      // Return any errors with 400 status
-      return res.status(400).json(errors)
-    }
-
-    Portfolio.findOne({ user: req.user.id })
-      .then(portfolio => {
-        const newExp = {
-          title: req.body.title,
-          company: req.body.company,
-          location: req.body.location,
-          from: req.body.from,
-          to: req.body.to,
-          current: req.body.current,
-          description: req.body.description
-        }
-
-        // Add to exp array
-        portfolio.experience.unshift(newExp)
-
-        portfolio
-          .save()
-          .then(portfolio => res.json(portfolio))
-          .catch(err => res.status(404).json(err))
-      })
-      .catch(err => res.status(404).json(err))
-  }
-)
-
-// @route   DELETE api/portfolio/user_id/income
-// @desc    Delete experience from portfolio
-// @access  Private
-router.delete(
-  '/:user_id/income',
-  passport.authenticate('jwt', { session: false }),
-  (req, res) => {
-    Portfolio.findOne({ user: req.user.id })
-      .then(portfolio => {
-        // Get remove index
-        const removeIndex = portfolio.experience
-          .map(item => item.id)
-          .indexOf(req.params.exp_id)
-
-        // Splice out of array
-        portfolio.experience.splice(removeIndex, 1)
-
-        // Save
-        portfolio.save().then(portfolio => res.json(portfolio))
-      })
-      .catch(err => res.status(404).json(err))
-  }
+  (req, res) => updatePortfolio(req, res)
 )
 
 // @route   DELETE api/portfolio/user_id
 // @desc    Delete portfolio and user account
 // @access  Private
 router.delete(
-  '/:user_id',
+  '/:portfolio_id',
   passport.authenticate('jwt', { session: false }),
-  (req, res) => {
-    Portfolio.findOneAndRemove({ user: req.user.id }).then(() => {
-      User.findOneAndRemove({ _id: req.user.id }).then(() =>
-        res.json({ success: true })
-      )
-    })
-  }
+  (req, res) => deletePortfolio(req, res)
 )
 
 module.exports = router
