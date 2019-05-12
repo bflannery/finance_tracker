d0d26f66935b1d29a1cc89132a4968d7c2b80e3a
Brian Flannery <bflannery66@email.com>
2019-05-12 11:48:09 -0500

    added local mongo uri

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 // DB Config
-const db = require('./config/keys').mongoURI
+const db = require('./config/keys').mongoLocalURI
 
 // MongoDB options
 const options = {
